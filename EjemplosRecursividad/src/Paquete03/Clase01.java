3f79cb580d3f5e810af713e6cb32b2e713103cc1
Carlos Valladares <csvalladares2@utpl.edu.ec>
2024-01-11 15:52:23 -0500

    Trabajo

---
+++ EjemplosRecursividad/src/Paquete03/Clase01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete03;
+
+/**
+ *
+ * @author vcarl
+ */
+public class Clase01 {
+    public static void main(String[] args) {
+        
+    }
+    
+    public static String [][] creadorCadenas(){
+       String [][] cadenas = {{"LOJA","CUENCA","QUITO"},
+           {"GUAYAQUIL","ZAMORA","MACHALA"}};
+       return cadenas;
+    }   
+}
