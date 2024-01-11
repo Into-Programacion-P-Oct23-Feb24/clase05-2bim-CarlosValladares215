8afea5f1a080ff1937cd99415a9e740055865e96
Carlos Valladares <csvalladares2@utpl.edu.ec>
2024-01-11 17:25:04 -0500

    Trabajo

--- EjemplosRecursividad/src/paquete02/Ejemplo06.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo06.java
@@ -16,10 +16,16 @@ public class Ejemplo06 {
         // [1][1]
         // hasta el final
         
+        // Este ciclo repetitivo llama directamente a la funcion, dandole
+        // directamente los valores de los arreglo
         for (int i = 0; i < valor1.length; i++) {
             System.out.printf("El valor es: %d\n", 
                     obtenerSuma(valor1[i], valor2[i]));
         }
+        
+        // Este otro ciclo repetitivo, primeramenete se encarga de guardar los
+        // los valores del arreglo en variables de tipo entero para usarlas
+        // en la funcion
         
         for (int i = 0; i < valor1.length; i++) {
             int num1 = valor1[i];
@@ -27,7 +33,7 @@ public class Ejemplo06 {
             System.out.printf("El valor es: %d\n", 
                     obtenerSuma(num1, num2));
         }
-        
+        // en este caso guarda el valor de la funcion para presentarla
         for (int i = 0; i < valor1.length; i++) {
             int num1 = valor1[i];
             int num2 = valor2[i];
